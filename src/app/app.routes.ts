081f481f5e74541d0c7f4f7ef9c5f9601c861796
manuvariego <73005152+manuvariego@users.noreply.github.com>
2024-09-05 19:20:51 -0300

    add: login button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UsersComponent } from './pages/users/users.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
 
-    {path: '', component: HomeComponent},
-    {path: 'users', component: UsersComponent},
-    {path: '**', redirectTo: '', pathMatch: 'full'}
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 
 ];
--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -10,18 +10,18 @@ export class ApiServiceService {
   private _http = inject(HttpClient);
   private apiUrl = 'http://localhost:3000/api/users';
 
-  getUsers(): Observable<any[]>{
+  getUsers(): Observable<any[]> {
     return this._http.get<any[]>(this.apiUrl);
   }
 
-  getUser(): Observable<any>{
+  getUser(): Observable<any> {
     return this._http.get<any>(this.apiUrl)
   }
 
   createUser(user: any): Observable<any> {
     return this._http.post<any>(this.apiUrl, user);
-  } 
-  
+  }
+
   delete(userId: string): Observable<any> {
     return this._http.delete(`${this.apiUrl}/${userId}`);
   }
@@ -30,9 +30,9 @@ export class ApiServiceService {
     return this._http.put(`${this.apiUrl}/${user.id}`, user);
   }
 
-  
 
-  } 
+
+}
 
 
 
