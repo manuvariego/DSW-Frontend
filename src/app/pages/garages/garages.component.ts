5680f967c3abfbd4000db48fcdf79e3a4950a4bd
Yamila <yamiladonzino51@gmail.com>
2024-10-26 21:39:33 -0300

    cambie garage

--- src/app/pages/garages/garages.component.ts
+++ src/app/pages/garages/garages.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { isArray } from 'node:util';
 import { GaragesService } from '../../services/garages.service.js';
+import { LocationsService } from '../../services/locations.service.js';
 
 @Component({
   selector: 'app-garages',
@@ -12,6 +13,14 @@ import { GaragesService } from '../../services/garages.service.js';
   styleUrl: './garages.component.css'
 })
 export class GaragesComponent {
+  currentSection: string = 'initial';
+  garageList: any[] = [];
+  private _apiservice = inject(GaragesService);
+  private _locationService = inject(LocationsService);
+  garageCreado = false;;
+  garageCuit: any = '';
+  aGarage: any = null;
+  editingGarage: any = null;
 
   garageData = {
 
@@ -20,97 +29,122 @@ export class GaragesComponent {
     password: '',
     address: '',
     phoneNumber: '',
-    email:'',
-    priceHour: 0,
+    email: '',
     location: '',
 
   }
 
-  garageCuit: any = ''
+  ngOnInit() {
+    this.loadLocations(); // Cargar las localidades al iniciar
+  }
 
-  modificarGarage(garage:any){
 
-   this.currentSection = 'editGarage';
-   this.editingGarage = { ...garage };
+
+  modificarGarage(garage: any) {
+
+    this.currentSection = 'editGarage';
+    this.editingGarage = { ...garage };
 
   }
 
-  currentSection: string = 'initial'
-
-  garageList: any[] = []
-
-// falta especificar que localidad tiene el garage
-
- private _apiservice = inject(GaragesService)
 
 
- createGarage() {
-  this._apiservice.createGarage(this.garageData).subscribe({
-    next: (response) => {
-      console.log('Cochera creada exitosamente:', response);
-    },
-    error: (error) => {
-      console.error('Error al crear la chochera:', error);
-    }
-  });
-}
 
-aGarage: any = null
-
-
-getGarage(){
-  this._apiservice.getGarage(this.garageCuit).subscribe(
-    (garage: any) => {
-      this.currentSection = "geTaGarageTable"
-      console.log(garage)
-      this.aGarage = garage
-      this.garageCuit = ''
-    },
-    (error) => {
-      console.error('Error al obtener un Garage', error);
-      // Aquí podrías mostrar un mensaje de error, por ejemplo usando alert o alguna librería como Toastr
-      alert('El garage no existe o ocurrió un error al obtener la información.');
-    }
-  );
-}
-
- getGarages(){
-  this._apiservice.getGarages().subscribe((data: any[])=>{
-
-    if(Array.isArray(data)){
-      this.garageList = data 
-
-    } else{
-
-      this.garageList = [data]
+  createGarage(form: NgForm) {
+    if (form.invalid) {
+      Object.keys(form.controls).forEach(field => {
+        const control = form.controls[field];
+        control.markAsTouched({ onlySelf: true });
+      });
+      return;
     }
 
-    
-    console.log(data)
-  })
- }
+    this._apiservice.createGarage(this.garageData).subscribe({
+      next: (response) => {
+        console.log('Garage creado exitosamente:', response);
+        this.garageCreado = true;
+        this.showSection('initial');
+        form.resetForm();
 
- deleteGarage(garageCuit: string , tipo: boolean) {
-  const confirmation = confirm('¿Está seguro de que desea eliminar esta cochera?');
-  if (!confirmation) {
-    return; // Si el usuario cancela, no hacemos nada
+        // Oculta el mensaje después de 3 segundos
+        setTimeout(() => {
+          this.garageCreado = false;
+        }, 3000);
+      },
+      error: (error) => {
+        console.error('Error al crear usuario:', error);
+      }
+    });
+
+
+
+
+    this._apiservice.createGarage(this.garageData).subscribe({
+      next: (response) => {
+        console.log('Cochera creada exitosamente:', response);
+      },
+      error: (error) => {
+        console.error('Error al crear la chochera:', error);
+      }
+    });
   }
 
-  this._apiservice.deleteGarage(garageCuit).subscribe({
-    next: (response) => {
-      console.log('Cochera eliminada exitosamente', response);
-      this.getGarages(); // Refrescar la lista de cocheras
-    },
-    error: (error) => {
-      console.error('Error al eliminar la cochera', error);
-    }
-  });
-  
-  if(tipo = true){this.currentSection = 'initial'}
-}
 
-  editingGarage: any = null;
-  
+
+
+  getGarage() {
+    this._apiservice.getGarage(this.garageCuit).subscribe(
+      (garage: any) => {
+        this.currentSection = "geTaGarageTable"
+        console.log(garage)
+        this.aGarage = garage
+        this.garageCuit = ''
+      },
+      (error) => {
+        console.error('Error al obtener un Garage', error);
+        // Aquí podrías mostrar un mensaje de error, por ejemplo usando alert o alguna librería como Toastr
+        alert('El garage no existe o ocurrió un error al obtener la información.');
+      }
+    );
+  }
+
+  getGarages() {
+    this._apiservice.getGarages().subscribe((data: any[]) => {
+
+      if (Array.isArray(data)) {
+        this.garageList = data
+
+      } else {
+
+        this.garageList = [data]
+      }
+
+
+      console.log(data)
+    })
+  }
+
+  deleteGarage(garageCuit: string, tipo: boolean) {
+    const confirmation = confirm('¿Está seguro de que desea eliminar esta cochera?');
+    if (!confirmation) {
+      return; // Si el usuario cancela, no hacemos nada
+    }
+
+    this._apiservice.deleteGarage(garageCuit).subscribe({
+      next: (response) => {
+        console.log('Cochera eliminada exitosamente', response);
+        this.getGarages(); // Refrescar la lista de cocheras
+      },
+      error: (error) => {
+        console.error('Error al eliminar la cochera', error);
+      }
+    });
+
+    if (tipo = true) { this.currentSection = 'initial' }
+  }
+
+
+
   updateGarage() {
     const confirmation = confirm('¿Está seguro de que desea modificar esta cochera?');
     if (!confirmation) {
@@ -118,45 +152,40 @@ getGarage(){
     }
 
     this._apiservice.updateGarage(this.editingGarage).subscribe({
-      next: (response) =>{
-      console.log('Cochera actualizada exitosamente', response);
-      this.editingGarage = null; // Limpia la variable de edición
-      this.getGarages(); // Refresca la lista de usuarios
+      next: (response) => {
+        console.log('Cochera actualizada exitosamente', response);
+        this.editingGarage = null; // Limpia la variable de edición
+        this.getGarages(); // Refresca la lista de usuarios
       },
-    error: (error) => {
-      console.error('Error al actualizar la cochera', error);
-    }
-  });
+      error: (error) => {
+        console.error('Error al actualizar la cochera', error);
+      }
+    });
   }
 
 
-    // Método para actualizar la sección actual mostrada
-    showSection(section: string) {
-      this.currentSection = section;
+  // Método para actualizar la sección actual mostrada
+  showSection(section: string) {
+    this.currentSection = section;
 
-      if(section == 'getGarage'){
+    if (section == 'getGarage') {
 
-        this.getGarages()
-
-      }
+      this.getGarages()
 
     }
 
-    locations: Array<any> = []; // Array para almacenar las localidades
+  }
 
-    ngOnInit() {
-      this.loadLocations(); // Cargar las localidades al iniciar
-    }
+  locations: Array<any> = []; // Array para almacenar las localidades
 
-    loadLocations() {
-      // Simular llamada a un servicio para obtener las localidades
-      this.locations = [
-        { name: 'Buenos Aires' },
-        { name: 'Córdoba' },
-        { name: 'Santa Fe' },
-        { name: 'Mendoza' }
-      ];
-    }
-  
+
+
+  loadLocations() {
+    // Simular llamada a un servicio para obtener las localidades
+    this._locationService.getLocations().subscribe(data => {
+      this.locations = data;
+    });
+  }
+
 
 }
